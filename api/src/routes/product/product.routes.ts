6a65ccad9680a34f79daeb685f6825526983e960
Pablo Barbero <pablobarbero220@gmail.com>
2022-10-24 17:00:33 -0300

    feat product requests

--- api/src/routes/product/product.routes.ts
+++ api/src/routes/product/product.routes.ts
@@ -3,6 +3,10 @@ import * as productController from "./product.controller";
 
 const router = Router();
 
-router.get("/products", productController.allProducts);
+router.get("/products", productController.getAllProducts);
+
+router.get("/product/:id", productController.getProduct);
+
+router.post("/products", productController.createProduct);
 
 export default router;
